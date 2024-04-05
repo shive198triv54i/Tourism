0e52b4b8c786d7a876e01c5cbb42f5db012e088e
Shivesh <shiveshtrivedi159@gmail.com>
2024-04-05 15:11:51 +0530

    my code is working

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 //temporary souls firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "travel-and-tourism-50943.firebaseapp.com",
-  projectId: "travel-and-tourism-50943",
-  storageBucket: "travel-and-tourism-50943.appspot.com",
-  messagingSenderId: "23151456147",
-  appId: "1:23151456147:web:4fdfb8c5a81fb8cc3a4266",
-  
+  authDomain: "tour-c17ae.firebaseapp.com",
+    projectId: "tour-c17ae",
+    storageBucket: "tour-c17ae.appspot.com",
+      messagingSenderId: "560842783645",
+      appId: "1:560842783645:web:b9f6c15484bca6957d8d74",
+      measurementId: "G-7QYHDPC1V4"
 
 
   // authDomain: "travel-and-tourism-50943.firebaseapp.com",
